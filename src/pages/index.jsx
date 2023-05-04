4cd3f4dd9fa98eba3a98d20091899ae4c1fd174e
apo-star <apolle.world@gmail.com>
2023-05-04 19:04:22 -0300

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Donuts Story</title>
+      <meta name='viewport' content='width=device-width, initial-scale=1'/>
+    </Head>
+    <main></main>
+    </>
+
+
+    
+  )
+}
